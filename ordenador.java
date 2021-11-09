b55e0228860a8b8760ebad0fb2d848e83a11dbe1
Pablo <pablovivasgarcia1@gmail.com>
2021-11-09 09:46:16 +0100

    01-Commit inicial

---
+++ ordenador.java
@@ -0,0 +1,37 @@
+public class ordenador {
+    // Atributos
+    private String procesador;
+    private int memoriaRAM;
+    private boolean ventiladoresFuncionando;
+    
+    // Construcctor
+    public ordenador(String nombreProcesador, int tamañoMemoriaRAM) {
+        procesador = nombreProcesador;
+        memoriaRAM = tamañoMemoriaRAM;
+        ventiladoresFuncionando = true;
+    }
+    
+    // Gasto energia
+    public double gastoEnergia(int minutosEncendido) {
+        double gastoTotal= (1.2 * minutosEncendido) + (1 * minutosEncendido) + (0.5 * minutosEncendido);
+        return gastoTotal;
+    }
+    
+    public void botonEncendido() {
+        ventiladoresFuncionando = !ventiladoresFuncionando;
+    }
+    
+    // Detalles
+    public void imprimirDatos() {
+        System.out.println("Procesador: " + procesador + " | RAM: " + memoriaRAM + " | Ventiladores: " + ventiladoresFuncionando);
+    }
+    
+    public void estado() {
+        if (ventiladoresFuncionando == true) {
+            System.out.println("Ventiladores funcionando");
+        }
+        else {
+            System.out.println("Ventiladores apagados");
+        }
+    }
+}
